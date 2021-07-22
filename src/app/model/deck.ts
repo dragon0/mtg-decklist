f13549fe96097501840dbb40676d1d4b15812e36
Raven <sravendragon0@hotmail.com>
2021-07-21 19:14:21 -0700

    Convert model to using constructor

--- src/app/model/deck.ts
+++ src/app/model/deck.ts
@@ -1,37 +1,193 @@
 
 export abstract class Card {
-  name: string = "";
-  type: string = "";
-  type_line: string = "";
-  oracle_text: string = "";
-  scryfall_uri: string = "";
-  scryfall_user_uri: string = "";
-  image_uri: string = "";
+  constructor(
+    public name: string,
+    public type: string,
+    public type_line: string,
+    public oracle_text: string,
+    public scryfall_api_uri: string,
+    public scryfall_user_uri: string,
+    public image_uri: string,
+  ){}
 };
 
 export class LandCard extends Card {
+  constructor(
+    public name: string,
+    public type_line: string,
+    public oracle_text: string,
+    public scryfall_api_uri: string,
+    public scryfall_user_uri: string,
+    public image_uri: string,
+  ){
+    super(
+      name,
+      'land',
+      type_line,
+      oracle_text,
+      scryfall_api_uri,
+      scryfall_user_uri,
+      image_uri,
+    )
+  }
 }
 
-export class InstantCard extends Card {
+export abstract class SpellCard extends Card {
+  constructor(
+    public name: string,
+    public type: string,
+    public type_line: string,
+    public oracle_text: string,
+    public scryfall_api_uri: string,
+    public scryfall_user_uri: string,
+    public image_uri: string,
+    public cmc: number,
+    public mana_cost: string,
+  ){
+    super(
+      name,
+      type,
+      type_line,
+      oracle_text,
+      scryfall_api_uri,
+      scryfall_user_uri,
+      image_uri,
+    )
+  }
 }
 
-export class SorceryCard extends Card {
+export class InstantCard extends SpellCard {
+  constructor(
+    public name: string,
+    public type_line: string,
+    public oracle_text: string,
+    public scryfall_api_uri: string,
+    public scryfall_user_uri: string,
+    public image_uri: string,
+    public cmc: number,
+    public mana_cost: string,
+  ){
+    super(
+      name,
+      'instant',
+      type_line,
+      oracle_text,
+      scryfall_api_uri,
+      scryfall_user_uri,
+      image_uri,
+      cmc,
+      mana_cost,
+    )
+  }
 }
 
-export class CreatureCard extends Card {
-  cmc: number = 0;
-  mana_cost: string = "";
-  power: string = "";
-  toughness: string = "";
+export class SorceryCard extends SpellCard {
+  constructor(
+    public name: string,
+    public type_line: string,
+    public oracle_text: string,
+    public scryfall_api_uri: string,
+    public scryfall_user_uri: string,
+    public image_uri: string,
+    public cmc: number,
+    public mana_cost: string,
+  ){
+    super(
+      name,
+      'sorcery',
+      type_line,
+      oracle_text,
+      scryfall_api_uri,
+      scryfall_user_uri,
+      image_uri,
+      cmc,
+      mana_cost,
+    )
+  }
 }
 
-export class PlaneswalkerCard extends Card {
-  loyalty: string = "";
+export class ArtifactCard extends SpellCard {
+  constructor(
+    public name: string,
+    public type_line: string,
+    public oracle_text: string,
+    public scryfall_api_uri: string,
+    public scryfall_user_uri: string,
+    public image_uri: string,
+    public cmc: number,
+    public mana_cost: string,
+  ){
+    super(
+      name,
+      'artifact',
+      type_line,
+      oracle_text,
+      scryfall_api_uri,
+      scryfall_user_uri,
+      image_uri,
+      cmc,
+      mana_cost,
+    )
+  }
+}
+
+export class CreatureCard extends SpellCard {
+  constructor(
+    public name: string,
+    public type_line: string,
+    public oracle_text: string,
+    public scryfall_api_uri: string,
+    public scryfall_user_uri: string,
+    public image_uri: string,
+    public cmc: number,
+    public mana_cost: string,
+    public power: string,
+    public toughness: string,
+  ){
+    super(
+      name,
+      'creature',
+      type_line,
+      oracle_text,
+      scryfall_api_uri,
+      scryfall_user_uri,
+      image_uri,
+      cmc,
+      mana_cost,
+    )
+  }
+}
+
+export class PlaneswalkerCard extends SpellCard {
+  constructor(
+    public name: string,
+    public type_line: string,
+    public oracle_text: string,
+    public scryfall_api_uri: string,
+    public scryfall_user_uri: string,
+    public image_uri: string,
+    public cmc: number,
+    public mana_cost: string,
+    public loyalty: string,
+  ){
+    super(
+      name,
+      'planeswalker',
+      type_line,
+      oracle_text,
+      scryfall_api_uri,
+      scryfall_user_uri,
+      image_uri,
+      cmc,
+      mana_cost,
+    )
+  }
 }
 
 export class Deck {
-  name: string = "";
-  id: number = 0;
-  cards: Card[] = [];
+  constructor(
+    public name: string,
+    public cards: Card[],
+  ){}
 }
 
